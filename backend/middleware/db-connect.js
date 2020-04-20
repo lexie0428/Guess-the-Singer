37c975493029a7ba0cd12cc99e5807d78de83fab
Aleksandra <lexie0428@yandex.ru>
2020-04-20 16:28:04 +0300

    herocu

--- backend/middleware/db-connect.js
+++ backend/middleware/db-connect.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/music", {
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
